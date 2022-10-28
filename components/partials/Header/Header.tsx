6a3c7e96afea1ce9e22172f024209070473e62c5
ToyaKasai <kasaitoya@team-lab.com>
2022-10-29 04:42:53 +0900

    fix: header title

--- components/partials/Header/Header.tsx
+++ components/partials/Header/Header.tsx
@@ -6,7 +6,7 @@ import styles from "./Header.module.scss";
 const Header: FC = () => {
   return (
     <header className={styles["header-container"]}>
-      <p className={styles["title"]}>Header</p>
+      <p className={styles["title"]}>Next13 tutorial</p>
       <nav className={styles["navigation"]}>
         <Link className={styles["link"]} href="/">
           nav
