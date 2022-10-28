c2beab734efc8dff5edd791c6a0f78087a2c0bac
ToyaKasai <kasaitoya@team-lab.com>
2022-10-29 04:42:03 +0900

    feat: add header component

---
+++ components/partials/Header/Header.tsx
@@ -0,0 +1,25 @@
+import { FC } from "react";
+import Link from "next/link";
+
+import styles from "./Header.module.scss";
+
+const Header: FC = () => {
+  return (
+    <header className={styles["header-container"]}>
+      <p className={styles["title"]}>Header</p>
+      <nav className={styles["navigation"]}>
+        <Link className={styles["link"]} href="/">
+          nav
+        </Link>
+        <Link className={styles["link"]} href="/">
+          nav
+        </Link>
+        <Link className={styles["link"]} href="/">
+          nav
+        </Link>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
